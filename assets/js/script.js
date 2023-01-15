776b0d9a7a2342ce2cef0737ed6b5d68b3f548d7
Luan Cordeiro <luam.lc19@gmail.com>
2023-01-15 10:53:51 -0300

    show modal info

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,7 +12,10 @@ function formatPrice(price) {
 }
 
 function closeModal() {
-    c('.pizzaWindow').style.display = 'none'; 
+    c('.pizzaWindow').style.opacity = 0;
+    setTimeout(() => {
+        c('.pizzaWindow').style.display = 'none';
+    }, 500);
 }
 
 pizzaJson.map((item, index) => {
@@ -25,8 +28,31 @@ pizzaJson.map((item, index) => {
     pizzaModels.querySelector('.pizza-item--desc').innerHTML = item.description;
     pizzaModels.querySelector('a').addEventListener('click', (e) => {
         e.preventDefault();
+        let key = e.target.closest('.pizza-item').getAttribute('data-key');
+        modalQt = 1;
+        modalKey = key;
         c('.pizzaWindow').style.display = 'flex';
-    })
+
+        c('.pizzaBig img').src = pizzaJson[key].img;
+        c('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
+        c('.PizzaInfo--desc').innerHTML = pizzaJson[key].description;
+        c('.pizzaInfo--actualPrice').innerHTML = formatPrice(pizzaJson[key].price);
+        c('.pizzaInfo--size.selected').classList.remove('selected');
+
+        cs('.pizzaInfo--size').forEach((size, sizeIndex) => {
+            if (sizeIndex == 2) {
+                size.classList.add('selected')
+            }
+            size.querySelector('span').innerHTML = `${pizzaJson[key].sizes[sizeIndex]}`;
+        });
+        c('.pizzaInfo--qt').innerHTML = modalQt;
+
+        c('.pizzaWindow').style.opacity = 0;
+        c('.pizzaWindow').style.display = 'flex';
+        setTimeout(() => {
+            c('.pizzaWindow').style.opacity = 1;
+        }, 200)
+    });
 
     c('main .container').appendChild(pizzaModels);
 });
